f26f11c411b8d849fc7ddc3f9e3cd7ec87655304
Ritwik Das <ritwikvd@gmail.com>
2022-06-10 13:03:44 +0530

    Synchronising from up

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,30 +1,27 @@
 local creategroup = vim.api.nvim_create_augroup
 local createcmd = vim.api.nvim_create_autocmd
 
-local syncgroup = creategroup("bin_dotfiles_sync", {clear = true})
 createcmd("BufWritePost", {
-pattern = "~/.gitconfig,~/.vimrc,~/.prettierrc,~/.zshrc,~/.config/nvim/**/*,~/.config/starship.toml,~/.tmux.conf,/usr/local/bin/*",
-group = syncgroup,
-command = "silent! !up %:p"
+-- pattern = "~/.gitconfig,~/.vimrc,~/.prettierrc,~/.zshrc,~/.config/nvim/**/*,~/.config/starship.toml,~/.tmux.conf,/usr/local/bin/*",
+pattern = {"*", "~/.gitconfig"},
+group = creategroup("bin_dotfiles_sync", {clear = true}),
+command = "!up %:p"
 })
 
-local prettiergroup = creategroup("prettier", {clear = true})
 createcmd("BufWritePost", {
 pattern = "~/Desktop/personal/pushowl/**/*",
-group = prettiergroup,
-command = "silent! execute \"!npx --silent prettier --write --loglevel silent --config ~/.prettierrc %:p\" | redraw!"
+group = creategroup("prettier", {clear = true}),
+command = "execute \"!npx --silent prettier --write --loglevel silent --config ~/.prettierrc %:p\" | redraw!"
 })
 
-local nerdtreegroup = creategroup("nerdtree", {clear = true})
 createcmd("FileType", {
 pattern = "nerdtree",
-group = nerdtreegroup,
+group = creategroup("nerdtree", {clear = true}),
 command = "setlocal rnu"
 })
 
-local eslintgroup = creategroup("eslint", {clear = true})
 createcmd("BufWritePre", {
 pattern = "*.tsx,*.ts,*.jsx,*.js",
-group = eslintgroup,
+group = creategroup("eslint", {clear = true}),
 command = "EslintFixAll"
 })
