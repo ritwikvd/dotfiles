c2befae7ac69c85a8068779f6ddafa501b063899
Ritwik Das <ritwikvd@gmail.com>
2022-06-10 12:25:50 +0530

    Synchronising from up

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,1 +1,13 @@
-local bin_dotfiles_sync = vim.api.nvim_create_augroup("bin_dotfiles_sync", {clear = true})
+local syncgroup = vim.api.nvim_create_augroup("bin_dotfiles_sync", {clear = true})
+vim.api.nvim_create_autocmd("BufWritePost", {
+pattern = "~/.gitconfig,~/.vimrc,~/.prettierrc,~/.zshrc,~/.config/nvim/**/*,~/.config/starship.toml,~/.tmux.conf,/usr/local/bin/*",
+group = syncgroup,
+command = "silent! !up %:p"
+})
+
+local prettiergroup = vim.api.nvim_create_augroup("prettier", {clear = true})
+vim.api.nvim_create_autocmd("BufWritePost", {
+pattern = "~/Desktop/personal/pushowl/**/*",
+group = prettiergroup,
+command = "silent! execute \"!npx --silent prettier --write --loglevel silent --config ~/.prettierrc %:p\" | redraw!"
+})
