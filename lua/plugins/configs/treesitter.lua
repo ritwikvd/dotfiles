6374bc293494943c3689afc211f4003e7024c3a2
Ritwik Das <ritwikvd@gmail.com>
2022-06-11 08:53:06 +0530

    Synchronising from up

--- lua/plugins/configs/treesitter.lua
+++ lua/plugins/configs/treesitter.lua
@@ -1,6 +1,7 @@
 local configs = require("nvim-treesitter.configs")
 
 return function ()
+    print("inside treesitter with configs", vim.inspect(configs))
     configs.setup({
         context_commentstring = {enable = true},
         autotag = {enable = true}
