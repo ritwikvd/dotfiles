784c50993efb14e4dd7d2b49f418c68cf393e411
Ritwik Das <ritwikvd@gmail.com>
2022-06-11 15:06:54 +0530

    Synchronising from up

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -8,11 +8,11 @@ local home = "/Users/ritwik"
 local patternstring = string.gsub("~/.config/**/*,~/.gitconfig,~/.vimrc,~/.prettierrc,~/.zshrc,~/.tmux.conf,/usr/local/bin/*", "~", home)
 local patternlist = vim.split(patternstring, ",")
 
-createcmd("BufWritePost", {
-pattern = patternlist,
-group = creategroup("bin_dotfiles_sync"),
-command = "silent! !up %:p"
-})
+-- createcmd("BufWritePost", {
+-- pattern = patternlist,
+-- group = creategroup("bin_dotfiles_sync"),
+-- command = "silent! !up %:p"
+-- })
 
 createcmd("BufWritePost", {
 pattern = home.."/Desktop/personal/pushowl/**/*",
