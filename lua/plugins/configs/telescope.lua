a68811a3fe3b388fb6d355da1db0ca30c3b04e98
Ritwik Das <ritwikvd@gmail.com>
2022-06-11 10:45:04 +0530

    Synchronising from up

---
+++ lua/plugins/configs/telescope.lua
@@ -0,0 +1,9 @@
+return function ()
+    local telescope = require("telescope")
+
+    telescope.setup({ defaults = {mappings = {n = {["<leader>v"] = "select_vertical"}}}, extensions = { ["ui-select"] = { require("telescope.themes").get_dropdown {} } }})
+
+    telescope.load_extension("ui-select")
+    telescope.load_extension('fzf')
+
+end
