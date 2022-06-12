80fcdd6358531acf5407956e9ddd1e478a988278
Ritwik Das <ritwikvd@gmail.com>
2022-06-12 10:38:35 +0530

    Synchronising from up

--- lua/plugins/configs/feline.lua
+++ lua/plugins/configs/feline.lua
@@ -14,11 +14,11 @@ return function ()
             name = require('feline.providers.vi_mode').get_mode_highlight_name(),
             fg = require('feline.providers.vi_mode').get_mode_color(),
             style = 'bold'
-        },
-        opts = {
-            show_mode_name = true
         }
-    end
+    end,
+    opts = {
+        show_mode_name = true
+    }
     }
 
     table.insert(activeleft, mode)
