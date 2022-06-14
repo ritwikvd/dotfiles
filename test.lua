b25367a7356abd8eeaa9243c5ef793d8d06e5168
Ritwik Das <ritwikvd@gmail.com>
2022-06-14 11:56:21 +0530

    Synchronising from up

--- test.lua
+++ test.lua
@@ -1,3 +1,4 @@
-vim.fn.setqflist(table.sort(vim.fn.getqflist(), function(a,b) 
+table.sort(vim.fn.getqflist(), function(a,b) 
 return b.bufnr > a.bufnr
-end))
+end)
+vim.fn.setqflist(vim.fn.getqflist())
