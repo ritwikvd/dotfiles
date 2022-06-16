e537a304cdddbf3c539671652077bb795412c1eb
Ritwik Das <ritwikvd@gmail.com>
2022-06-16 08:45:03 +0530

    Synchronising from up

--- lua/remaps.lua
+++ lua/remaps.lua
@@ -33,7 +33,7 @@ nset("<c-n>", "g:NERDTree.IsOpen() ? ':NERDTreeClose<cr>' : @% == '' ? ':NERDTre
 -- xset("J", "<cmd>m '>+1<cr>gv", {desc = "Move Line Down"})
 -- xset("K", "<cmd>m '<-2<cr>gv=gv", {desc = "Move Line Up"})
 xset("<leader>r", function ()
-    vim.cmd("visual \"zy")
+    vim.cmd("normal \"zy")
     vim.ui.input({prompt = "Enter replacement string: "}, function (input)
         local reg = vim.fn.getreg("z")
         if not reg then return end
