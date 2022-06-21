61da0c9f4dbebb76f698ea8c370b9c0f437f7742
Ritwik Das <ritwikvd@gmail.com>
2022-06-21 21:20:36 +0530

    Delete lua directory

--- lua/commands.lua
+++
@@ -1,7 +0,0 @@
-vim.cmd([[
-colorscheme gruvbox
-hi CursorLineNr guifg=DarkMagenta
-
-xnoremap J :m '>+1<CR>gv=gv
-xnoremap K :m '<-2<CR>gv=gv
-]])
--- lua/plugins/configs/treesitter.lua
+++
@@ -1,6 +0,0 @@
-return function ()
-    require("nvim-treesitter.configs")
-    .setup({
-        context_commentstring = {enable = true},
-    })
-end
