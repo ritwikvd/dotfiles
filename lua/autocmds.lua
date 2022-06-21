b87a2ad40ffbc2789a2e52c40b5b377691cb7692
Ritwik Das <ritwikvd@gmail.com>
2022-06-21 14:13:06 +0530

    Synchronising from up

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -35,5 +35,5 @@ command = "EslintFixAll"
 -- createcmd("BufWritePost", {
 -- pattern = "*.c,*.cpp",
 -- group = creategroup("clang-tidy"),
--- command = "silent! execute \"!/opt/homebrew/Cellar/llvm/13.0.1_1/bin/clang-tidy % --fix --fix-errors\" | redraw!"
+-- command = "silent! execute \"!clang-tidy % --fix --fix-errors\" | redraw!"
 -- })
