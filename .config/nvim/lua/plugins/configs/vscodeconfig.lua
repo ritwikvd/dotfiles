dc309fc9a1daa8dec5bc9d37a722d835ca0650ae
Ritwik Das <ritwikvd@gmail.com>
2022-08-22 12:54:32 +0530

    Synchronising from up

---
+++ .config/nvim/lua/plugins/configs/vscodeconfig.lua
@@ -0,0 +1,9 @@
+return function ()
+    local c = require("vscode.colors")
+    require("vscode")
+    .setup({
+        group_overrides = {
+            Cursor = { fg=c.vscDarkBlue, bg=c.vscLightGreen, bold=true }
+        }
+    })
+end
