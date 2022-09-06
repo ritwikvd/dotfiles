158a9cccdcd12fe3507c0f035440575d0a0aaac0
Ritwik Das <ritwikvd@gmail.com>
2022-09-06 11:50:26 +0530

    Synchronising from up

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -38,11 +38,12 @@ group = creategroup("clang-tidy"),
 command = "silent! execute \"!clang-tidy % --fix-errors\" | redraw!"
 })
 
-createcmd("BufEnter", {
+createcmd({ "BufEnter", "BufWritePost" }, {
 pattern = "*.c,*.cpp",
 group = creategroup("clang-format"),
 command = "silent! execute \"!clang-format -i --style='file:/Users/ritwik/.clang-format' %\" | redraw!"
 })
+
 -- local previewpath = home.."/Desktop/personal/pushowl/dashboard/.github/actions/get-preview"
 
 createcmd("BufWritePost", {
