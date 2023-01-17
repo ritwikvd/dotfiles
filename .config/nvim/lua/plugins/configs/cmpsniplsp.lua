d5613379207b5d322a3b2bb0e61cc1001d8dbf6a
Ritwik Das <ritwikvd@gmail.com>
2023-01-17 15:06:15 +0530

    Synchronising from up

--- .config/nvim/lua/plugins/configs/cmpsniplsp.lua
+++ .config/nvim/lua/plugins/configs/cmpsniplsp.lua
@@ -49,7 +49,7 @@ return function ()
             Lua = {
                 runtime = { version = "LuaJIT", path = runtime_path },
                 diagnostics = { globals = {"vim"} },
-                workspace = { library = vim.api.nvim_get_runtime_file("", true) },
+                workspace = { library = vim.api.nvim_get_runtime_file("", true), checkThirdParty = false},
                 completion = { callSnippet = "Replace" },
                 telemetry = { enable = false },
             },
