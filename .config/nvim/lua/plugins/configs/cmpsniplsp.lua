766e4f57e0c0afbc8001e5c90480429508462515
Ritwik Das <ritwikvd@gmail.com>
2023-01-17 15:14:14 +0530

    Synchronising from up

--- .config/nvim/lua/plugins/configs/cmpsniplsp.lua
+++ .config/nvim/lua/plugins/configs/cmpsniplsp.lua
@@ -10,7 +10,8 @@ return function ()
 
     -- LSP
     local lspconfig = require("lspconfig")
-    local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+    -- local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+    local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
     capabilities.textDocument.completion.completionItem.snippetSupport = true
 
     lspconfig.eslint.setup{}
