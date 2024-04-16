ceaf268350c5384a5fd4d87e9fe0e6b1f7461cb3
Ritwik Das <ritwikvd@gmail.com>
2024-04-16 19:32:41 +0530

    Synchronising from up

--- .config/nvim/lua/plugins/configs/vscodeconfig.lua
+++ .config/nvim/lua/plugins/configs/vscodeconfig.lua
@@ -1,6 +1,7 @@
 return function ()
     local vscode = require("vscode")
     local colors = require("vscode.colors").get_colors()
+    print("inside setting up vscode colours")
 
     vscode.setup({
         group_overrides = {
