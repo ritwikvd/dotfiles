48599b92cdfaf0e0af1f3ce2df48bf971790b1c1
Ritwik Das <ritwikvd@gmail.com>
2024-04-16 19:35:28 +0530

    Synchronising from up

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -14,8 +14,6 @@ return require("packer").startup(function (use)
     use("preservim/nerdtree")
 
     -- Themes
-    require("vscode").load()
-    print("inside setting up vscode colours")
     use({ "Mofiqul/vscode.nvim", config = vscodeconfig})
 
     use("neovim/nvim-lspconfig")
